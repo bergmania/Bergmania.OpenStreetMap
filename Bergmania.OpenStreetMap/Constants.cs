7e9cf63a0e088bcf986ee63658069ef841f72d36
Bjarne Fyrstenborg <bf@mcb.dk>
2021-10-26 12:40:09 +0200

    Use map location icon for property editor

--- Bergmania.OpenStreetMap/Constants.cs
+++ Bergmania.OpenStreetMap/Constants.cs
@@ -3,7 +3,9 @@
     public static class Constants
     {
         public const string EditorAlias = "Bergmania.OpenStreetMap";
+        public const string EditorName = "Open Street Map";
         public const string EditorView = "~/App_Plugins/Bergmania.OpenStreetMap/bergmania.openstreetmap.html";
+        public const string EditorIcon = "icon-map-location";
         public const string TextStringView = "textstring";
     }
 }
