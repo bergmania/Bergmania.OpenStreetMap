773e15e5e8667c0855bc68b9b3ee434da7b85732
Bjarne Fyrstenborg <bjarne_fyrstenborg@hotmail.com>
2021-11-01 21:16:41 +0100

    Add search autocomplete

--- Bergmania.OpenStreetMap/App_Plugins/Bergmania.OpenStreetMap/bergmania.openstreetmap.controller.js
+++ Bergmania.OpenStreetMap/App_Plugins/Bergmania.OpenStreetMap/bergmania.openstreetmap.controller.js
@@ -1,14 +1,17 @@
-angular.module("umbraco")
-    .controller("Bergmania.OpenStreetMap.Controller", ["$scope", "$element",  function ($scope, $element) {
+﻿angular.module("umbraco")
+    .controller("Bergmania.OpenStreetMap.Controller", ["$scope", "$element", "$timeout",  function ($scope, $element, $timeout) {
+
         const vm = this;
+
+        vm.inputId = "osm_search_" + String.CreateGuid();
 
         vm.currentMarker = null;
 
-        function onInit(){
+        function onInit() {
 
             const initValue = $scope.model.value || $scope.model.config.defaultPosition || { marker : { latitude: 54.975556, longitude : -1.621667}, "boundingBox":{"southWestCorner":{"latitude":54.970495269313204,"longitude":-1.6278648376464846},"northEastCorner":{"latitude":54.97911600936982,"longitude":-1.609625816345215}}, zoom: 16};
             const tileLayer = $scope.model.config.tileLayer || 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
-            const tileLayerOptions = { attribution:$scope.model.config.tileLayerAttribution };
+            const tileLayerOptions = { attribution: $scope.model.config.tileLayerAttribution };
 
             vm.map = L.map($element.find("[data-openstreetmap]")[0])
                 .fitBounds(L.latLngBounds(L.latLng(initValue.boundingBox.southWestCorner.latitude, initValue.boundingBox.southWestCorner.longitude),
@@ -21,12 +24,18 @@ angular.module("umbraco")
             vm.map.on('zoomend', updateModel);
             vm.map.on('contextmenu', clearMarker);
 
-            if(initValue.marker){
-                vm.currentMarker = L.marker(L.latLng(initValue.marker.latitude, initValue.marker.longitude), {draggable:true,}).addTo(vm.map);
+            if (initValue.marker) {
+                vm.currentMarker = L.marker(L.latLng(initValue.marker.latitude, initValue.marker.longitude), { draggable: true }).addTo(vm.map);
             }
+
+            // Ensure DOM is ready
+            $timeout(function () {
+                initAutocompleteSearch();
+            });
         }
+
         function clearMarker() {
-            if(vm.currentMarker){
+            if (vm.currentMarker){
                 vm.currentMarker.remove(vm.map);
                 vm.currentMarker = null;
             }
@@ -38,24 +47,24 @@ angular.module("umbraco")
             clearMarker();
 
             vm.map.setView(e.latlng);
-            vm.currentMarker = L.marker(e.latlng, {draggable:true,}).addTo(vm.map);
+            vm.currentMarker = L.marker(e.latlng, { draggable: true }).addTo(vm.map);
 
             updateModel(e);
         }
 
-        function updateModel(){
+        function updateModel() {
 
             $scope.model.value = {};
 
             $scope.model.value.zoom = vm.map.getZoom();
 
-            if(!$scope.model.value.boundingBox){
+            if (!$scope.model.value.boundingBox) {
                 $scope.model.value.boundingBox = {};
             }
-            if(!$scope.model.value.boundingBox.southWestCorner){
+            if (!$scope.model.value.boundingBox.southWestCorner) {
                 $scope.model.value.boundingBox.southWestCorner = {};
             }
-            if(!$scope.model.value.boundingBox.northEastCorner){
+            if (!$scope.model.value.boundingBox.northEastCorner) {
                 $scope.model.value.boundingBox.northEastCorner = {};
             }
 
@@ -65,22 +74,115 @@ angular.module("umbraco")
             $scope.model.value.boundingBox.northEastCorner.latitude = northEastCorner.lat;
             $scope.model.value.boundingBox.northEastCorner.longitude = northEastCorner.lng;
             $scope.model.value.boundingBox.southWestCorner.latitude = southWestCorner.lat;
-            $scope.model.value.boundingBox.southWestCorner.longitude =southWestCorner.lng;
+            $scope.model.value.boundingBox.southWestCorner.longitude = southWestCorner.lng;
 
-            if(vm.currentMarker){
+            if (vm.currentMarker) {
                 const marker = vm.currentMarker.getLatLng();
 
-                if(!$scope.model.value.marker){
+                if (!$scope.model.value.marker) {
                     $scope.model.value.marker = {};
                 }
+
                 $scope.model.value.marker.latitude = marker.lat;
                 $scope.model.value.marker.longitude = marker.lng;
 
-            }else{
+            } else {
                 $scope.model.value.marker = null;
             }
-
-
         }
+
+        function initAutocompleteSearch() {
+
+            new Autocomplete(vm.inputId, {
+                selectFirst: true,
+                howManyCharacters: 2,
+
+                // onSearch
+                onSearch: ({ currentValue }) => {
+
+                    const limit = 5;
+                    const api = `https://nominatim.openstreetmap.org/search?format=geojson&limit=${limit}&q=${encodeURI(currentValue)}`;
+
+                    return new Promise((resolve) => {
+                        fetch(api)
+                            .then(response => response.json())
+                            .then(data => {
+                                resolve(data.features)
+                            })
+                            .catch(error => {
+                                console.error(error);
+                            })
+                    })
+                },
+                // nominatim GeoJSON format parse this part turns json into the list of
+                // records that appears when you type.
+                onResults: ({ currentValue, matches, template }) => {
+                    const regex = new RegExp(currentValue, 'gi');
+
+                    // if the result returns 0 we
+                    // show the no results element
+                    return matches === 0 ? template : matches
+                        .map((element) => {
+                            return `
+                              <li class="loupe">
+                                <p>
+                                  ${element.properties.display_name.replace(regex, (str) => `<b>${str}</b>`)}
+                                </p>
+                              </li> `;
+                        }).join('');
+                },
+
+                // we add an action to enter or click
+                onSubmit: ({ object }) => {
+                    console.log("object", object);
+                    const { display_name } = object.properties;
+                    const cords = object.geometry.coordinates;
+
+                    // custom id for marker
+                    const customId = Math.random();
+
+                    // create marker and add to map
+                    const marker = L.marker([cords[1], cords[0]], {
+                        title: display_name,
+                        id: customId,
+                        draggable: true
+                    })
+                    .addTo(vm.map)
+                    .bindPopup(display_name);
+
+                    // sets the view of the map
+                    vm.map.setView([cords[1], cords[0]]);
+
+                    // fit bounds
+                    vm.map.fitBounds([
+                        
+                    ]);
+
+                    vm.currentMarker = marker;
+
+                    // removing the previous marker
+                    vm.map.eachLayer(function (layer) {
+                        if (layer.options && layer.options.pane === "markerPane") {
+                            if (layer.options.id !== customId) {
+                                vm.map.removeLayer(layer);
+                            }
+                        }
+                    });
+
+                    updateModel();
+                },
+
+                // get index and data from li element after
+                // hovering over li with the mouse or using
+                // arrow keys ↓ | ↑
+                onSelectedItem: ({ index, element, object }) => {
+                    console.log('onSelectedItem:', index, element, object);
+                },
+
+                // the method presents no results element
+                noResults: ({ currentValue, template }) => template(`<li>No results found: "${currentValue}"</li>`),
+            });
+        }
+
         onInit();
     }]);
