f3c87c3fb81bb2df51d70600e3c2b8e579000560
Bjarke Berg <mail@bergmania.dk>
2024-04-11 20:58:06 +0200

    Prepared for v14 with new test site and renamed the legacy core package

--- Bergmania.OpenStreetMap.Core.Legacy/AutocompleteCssFile.cs
+++ Bergmania.OpenStreetMap.Core.Legacy/AutocompleteCssFile.cs
@@ -2,6 +2,12 @@
 
 namespace Bergmania.OpenStreetMap.Core
 {
+    /// <summary>
+    /// Represents the autocomplete CSS file.
+    /// </summary>
+    /// <remarks>
+    /// This class is used to register the autocomplete CSS file with the Umbraco backoffice.
+    /// </remarks>
     internal class AutocompleteCssFile : CssFile
     {
         public AutocompleteCssFile()
