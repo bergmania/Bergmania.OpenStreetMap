142fe52efb9c27d7cb2b9d7e74027f57e5f01b02
Bjarke Berg <mail@bergmania.dk>
2024-04-25 14:26:46 +0200

    cleanup

--- Bergmania.OpenStreetMap.Client/devops/build/copy-to-cms.js
+++
@@ -1,9 +0,0 @@
-import { cpSync, rmSync } from 'fs';
-
-const srcDir = './dist';
-const outputDir = '../Bergmania.OpenStreetMap.StaticAssets/wwwroot/App_Plugins/Bergmania.OpenStreetMap';
-
-rmSync(outputDir, { recursive: true, force: true });
-cpSync(srcDir, outputDir, { recursive: true });
-
-console.log('--- Copied build output to static assets successfully. ---');
